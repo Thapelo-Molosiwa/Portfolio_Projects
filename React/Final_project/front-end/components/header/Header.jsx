8b90b05590c9276e78b1c0adf0a3ef63bce52967
Isaac Thapelo Molosiwa <123638341+Thapelo-Molosiwa@users.noreply.github.com>
2024-06-07 13:34:24 +0200

    done with Navbar

--- React/Final_project/front-end/components/header/Header.jsx
+++ React/Final_project/front-end/components/header/Header.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import "../header/header.css"
+import "../header/header.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">MyLogo</div>
+      <div className="logo">
+        <a href="#home">
+          <div className='logolog'>
+          <img src='../components\images\Gemini_Generated_Image_1gm7v51gm7v51gm7-removebg-preview.png' alt="MyLogo" />
+        
+          </div>
+       </a>
+      </div>
       <div className="search-bar">
         <input type="text" placeholder="Search..." />
         <button type="submit">Go</button>
       </div>
       <nav className="nav">
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
+          
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
-      
     </header>
   );
 };
